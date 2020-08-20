f6f9a811211094e91982467bee0e7d0a0d3cae9e
Peter <petersantos000@gmail.com>
2020-08-20 01:51:07 -0400

    Added Create to Index

---
+++ Pages/ServiceTypes/Create.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GlimmerAuto.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GlimmerAuto
+{
+    public class CreateModel : PageModel
+    {
+        public ServiceType ServiceType { get; set; }
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}
--- Pages/ServiceTypes/Index.cshtml.cs
+++ Pages/ServiceTypes/Index.cshtml.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GlimmerAuto.Data;
+using GlimmerAuto.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace GlimmerAuto
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly ApplicationDbContext _db;
 
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IList<ServiceType> ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            ServiceType = await _db.ServiceType.ToListAsync();
+            return Page();
         }
     }
 }

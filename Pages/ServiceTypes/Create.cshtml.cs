5d2db2f344cb36881e6440186aa97d6708dfcdee
Peter <petersantos000@gmail.com>
2020-09-01 00:21:18 -0400

    Added Edit view

--- Pages/ServiceTypes/Create.cshtml.cs
+++ Pages/ServiceTypes/Create.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using GlimmerAuto.Data;
 using GlimmerAuto.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -10,10 +11,32 @@ namespace GlimmerAuto
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        [BindProperty]
         public ServiceType ServiceType { get; set; }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult OnGet()
         {
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.ServiceType.Add(ServiceType);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }

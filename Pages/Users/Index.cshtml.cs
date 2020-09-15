df84212b09fc20031cde46bba48a851d46bdede6
Peter <petersantos000@gmail.com>
2020-09-15 01:29:29 -0400

    Added Search criteria for users page model

--- Pages/Users/Index.cshtml.cs
+++ Pages/Users/Index.cshtml.cs
@@ -27,16 +27,52 @@ namespace GlimmerAuto.Pages.Users
         //public List<ApplicationUser> ApplicationUserList { get; set; }
         public UsersListViewModel UsersListVM { get; set; }
 
-        public async Task<IActionResult> OnGet(int productPage = 1)
+        public async Task<IActionResult> OnGet(int productPage = 1, string searchEmail=null, string searchName = null, string searchPhone = null)
         {
             UsersListVM = new UsersListViewModel()
             {
                 ApplicationUserList = await _db.ApplicationUser.ToListAsync()
-
             };
 
             StringBuilder param = new StringBuilder();
             param.Append("/Users?productPage=:"); // append the url and users
+
+            param.Append("&searchName=");
+            if (searchName != null)
+            {
+                param.Append(searchName);
+            }
+            param.Append("&searchEmail=");
+            if (searchEmail != null)
+            {
+                param.Append(searchEmail);
+            }
+            param.Append("&searchPhone=");
+            if (searchPhone != null)
+            {
+                param.Append(searchPhone);
+            }
+
+            // Filter ApplicationUserList
+            if (searchEmail != null)
+            {
+                UsersListVM.ApplicationUserList = await _db.ApplicationUser.Where(u => u.Email.ToLower().Contains(searchEmail.ToLower())).ToListAsync();
+            }
+            else
+            {
+                if (searchName != null)
+                {
+                    UsersListVM.ApplicationUserList = await _db.ApplicationUser.Where(u => u.Name.ToLower().Contains(searchName.ToLower())).ToListAsync();
+                }
+                else
+                {
+                    if (searchPhone != null)
+                    {
+                        UsersListVM.ApplicationUserList = await _db.ApplicationUser.Where(u => u.PhoneNumber.ToLower().Contains(searchPhone.ToLower())).ToListAsync();
+                    }
+                }
+            }
+
             var count = UsersListVM.ApplicationUserList.Count;
 
             UsersListVM.PageInfo = new PagingInfo
